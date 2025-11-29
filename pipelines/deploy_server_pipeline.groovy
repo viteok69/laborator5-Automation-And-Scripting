55fc4c6c99b97cf044d31e0f5bf542db93cf58da
viteok69 <victorbadia2003@gmail.com>
2025-11-29 14:40:33 +0200

    Fix: Disabled ANSIBLE_HOST_KEY_CHECKING to prevent SSH host key failure

--- pipelines/deploy_server_pipeline.groovy
+++ pipelines/deploy_server_pipeline.groovy
@@ -2,22 +2,35 @@ def PLAYBOOK_INSTALL = 'ansible/playbook-install.yaml'
 def ANSIBLE_HOSTS = 'ansible/hosts'
 
 pipeline {
-    agent { label 'ansible-agent' }
+    agent { label 'ansible-agent' } 
     options {
         skipDefaultCheckout true
     }
     stages {
         stage('Checkout Ansible Files') {
             steps {
-                git branch: 'main', url: 'https://github.com/viteok69/laborator5-Automation-And-Scripting' 
+                git branch: 'main', url: 'https://github.com/viteok69/laborator5-Automation-And-Scripting'
             }
         }
         stage('Deploy Test Server Infrastructure') {
             steps {
-                sshagent(['ANSIBLE_TO_TEST_SERVER_CREDENTIAL_ID']) {
-                    sh "ansible-playbook -i ${ANSIBLE_HOSTS} ${PLAYBOOK_INSTALL} --user=ansible"
+                script {
+                    sshagent(['ANSIBLE_TO_TEST_SERVER_CREDENTIAL_ID']) {
+                        
+                        echo "Running Ansible Playbook to configure Test Server (Apache2 + PHP-FPM)..."
+                        
+                        sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${ANSIBLE_HOSTS} ${PLAYBOOK_INSTALL} --user=ansible"
+                    }
                 }
             }
+        }
+    }
+    post {
+        success {
+            echo "Deployment Infrastructure successfully configured!"
+        }
+        failure {
+            echo "Infrastructure setup failed. Check Ansible logs."
         }
     }
 }
